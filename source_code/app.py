c994e00520b8ac3f218eeb47fff0195d33e56e9b
Claire Muwanguzi <31630227+Claire56@users.noreply.github.com>
2023-09-03 00:03:13 -0700

    Updated folder structure

--- source_code/app.py
+++ source_code/app.py
@@ -12,3 +12,28 @@ async def sum():
     return {"message": "Hello Peninah. This is the sum"}
 
 
+from fastapi import FastAPI
+from uvicorn import Server
+
+from uvicorn.config import Config
+from pyctuator.pyctuator import Pyctuator
+
+
+app_name = "FastAPI App with Pyctuator"
+app = FastAPI(title=app_name)
+
+
+@app.get("/")
+def hello():
+    return "Hello World!"
+
+
+Pyctuator(
+    app,
+    "FastAPI Pyctuator",
+    app_url="http://localhost:8000",
+    pyctuator_endpoint_url="http://host.docker.internal:8000/pyctuator",
+    registration_url="http://localhost:8080/instances"
+)
+
+Server(config=(Config(app=app, loop="asyncio"))).run()
---
+++ source_code/tests/health_test.py
@@ -0,0 +1,13 @@
+
+
+from fastapi import FastAPI
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello Peninah. This is the Actuator Project"}
+
+@app.get("/sum")
+async def sum():
+    return {"message": "Hello Peninah. This is the sum"}
